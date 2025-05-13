f8e005007986e5da1f2f520a410ef39b7f38304d
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-05-13 12:43:03 +0530

    fixes error

--- traffic-light/script.js
+++ traffic-light/script.js
@@ -1,20 +1,8 @@
-window.onload = function() {
-    const lights=[
-        document.getElementById('red'),
-        document.getElementById('yellow'),
-        document.getElementById('green'),
-    ]
-    document.getElementById('start').addEventListener('click', function(){
-        changeLight(lights)
-    });
-}
-
-let currentlight = 0;
+let currentLight = 0;
 const lights = document.querySelectorAll('.light');
 
 function changeLight() {
-
-    if(lights.length === 0) {
+    if (lights.length === 0) {
         console.error('No lights found');
         return;
     }
@@ -25,8 +13,10 @@ function changeLight() {
     });
 
     // Turn on the current light
-    lights[currentlight].classList.add('on');
+    lights[currentLight].classList.add('on');
 
     // Move to the next light
-    currentlight = (currentlight + 1) % lights.length;
+    currentLight = (currentLight + 1) % lights.length;
 }
+
+document.getElementById('start').addEventListener('click', changeLight);
