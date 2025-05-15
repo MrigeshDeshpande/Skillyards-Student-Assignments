6be2ebbffef3840eecfba750fbaa47a98175cd60
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-05-15 13:22:58 +0530

    todo app- basic addItem functionality

---
+++ todo-app/script.js
@@ -0,0 +1,20 @@
+const vishakha = document.getElementById("todo-input");
+const sachin = document.getElementById("todo-button");
+const jaadugar = document.getElementById("todo-list");
+
+let todos = {};
+
+function addTask(e){
+    e.preventDefault();
+    const taskText = vishakha.value.trim();
+    
+    //new-list-item
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+
+    //add-item-to-list
+    jaadugar.appendChild(listItem);
+
+    vishakha.value = "";
+}
+sachin.addEventListener("click", addTask);
